0a2b082f0b442d1e786a91f50f06e2f271639902
co1in <colinw@savix.net>
2013-09-13 19:05:24 -0400

    Started adding accelerometer smoothing

--- RobotController/src/main/java/com/nashobarobotics/robotcontroller/MainActivity.java
+++ RobotController/src/main/java/com/nashobarobotics/robotcontroller/MainActivity.java
@@ -9,51 +9,72 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
+import android.graphics.PointF;
 
 public class MainActivity extends Activity implements SensorEventListener
 {
     private TextView xText, yText, zText;
     private int nums = 0;
     private SensorManager sensorManager;
+	private ArrayList<PointF> accelerometerValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+		accelerometerValues = new ArrayList<PointF>();
         xText = (TextView)findViewById(R.id.xtext);
         yText = (TextView)findViewById(R.id.ytext);
         zText = (TextView)findViewById(R.id.ztext);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        new Thread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                try
-                {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-                MainActivity.this.runOnUiThread(new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        Toast.makeText(MainActivity.this, "" + nums, Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        }).start();
+		new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				while(true)
+				{
+					try
+					{
+						Thread.sleep(100);
+					}
+					catch (InterruptedException e)
+					{}
+					MainActivity.this.runOnUiThread(new Runnable()
+					{
+						@Override
+						public void run()
+						{
+							updateAccelerometer();
+						}
+					});
+				}
+			}
+		});
 
     }
 
-
+    public void updateAccelerometer()
+	{
+		float sumy = 0, sumz = 0;
+		
+		sumy *= 10f;
+		sumz *= 10f;
+		sumy = Math.round(sumy);
+		sumz = Math.round(sumz);
+		sumy /= 10f;
+		sumz /= 10f;
+		
+		yText.setText("y: " + sumy);
+		zText.setText("z: " + sumz);
+		accelerometerValues.clear();
+	}
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -73,13 +94,17 @@ public class MainActivity extends Activity implements SensorEventListener
                 values[i] = Math.round(values[i]);
                 values[i] /= 10f;
             }
-            //xText.setText("x: " + values[0]);
-            nums++;
-            yText.setText("y: " + values[1]);
-            zText.setText("z: " + values[2]);
+            //xText.setText("x: " + values[0])
+            //yText.setText("y: " + values[1]);
+            //zText.setText("z: " + values[2]);
         }
     }
 
+	public void getSmoothValues(float[] newValues, float[] oldValues)
+	{
+		
+	}
+	
     @Override
     public void onAccuracyChanged(Sensor sensor, int i)
     {
