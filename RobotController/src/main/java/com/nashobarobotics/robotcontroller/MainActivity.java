205998287abe3c6f2c65d1518e7fb74a05aa3e1b
co1in <colinw@savix.net>
2013-09-12 23:16:56 -0400

    Added accelerometer display

--- RobotController/src/main/java/com/nashobarobotics/robotcontroller/MainActivity.java
+++ RobotController/src/main/java/com/nashobarobotics/robotcontroller/MainActivity.java
@@ -1,15 +1,56 @@
 package com.nashobarobotics.robotcontroller;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener
+{
+    private TextView xText, yText, zText;
+    private int nums = 0;
+    private SensorManager sensorManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        xText = (TextView)findViewById(R.id.xtext);
+        yText = (TextView)findViewById(R.id.ytext);
+        zText = (TextView)findViewById(R.id.ztext);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                MainActivity.this.runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        Toast.makeText(MainActivity.this, "" + nums, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).start();
+
     }
 
 
@@ -19,5 +60,44 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            float[] values = sensorEvent.values;
+            for(int i = 0; i < 3; i++)
+            {
+                values[i] *= 10f;
+                values[i] = Math.round(values[i]);
+                values[i] /= 10f;
+            }
+            //xText.setText("x: " + values[0]);
+            nums++;
+            yText.setText("y: " + values[1]);
+            zText.setText("z: " + values[2]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i)
+    {
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 }
